1e963552324eca5f05d27bab3bea6178e6ee17b4
Muriszk <murilo.suzuki@sptech.school>
2025-11-29 11:40:36 -0300

    Shell script - adicionando mensagem dinamica para desenvolvedor poder dar commit com mensagem personalizada

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,13 @@
 echo "=== verificando situação atual  ==="
 git status 
 
+echo "=== adicione a mensagem"
+read -p "Digite a mensagem do commit" msg
+
 echo "=== Adicionando alterações ==="
 git add .
 
 echo "=== commitadno ==="
-git commit -m "commit"
+git commit -m "$msg"
 
 echo "finalizado!"
