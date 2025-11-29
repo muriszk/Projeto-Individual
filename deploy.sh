f2e255972e6e33bf45078d44dd74b63abc25179a
Muriszk <murilo.suzuki@sptech.school>
2025-11-29 12:11:04 -0300

    Shell script - Testando

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ echo "=== verificando situação atual  ==="
 git status 
 
 echo "=== adicione a mensagem"
-read -p "Digite a mensagem do commit" msg
+read -p " Digite a mensagem do commit" msg
 
 echo "=== Adicionando alterações ==="
 git add .
 
 echo "=== commitadno ==="
-git commit -m "$msg"
+git commit -m " $msg"
 
 echo "=== adicionando ao repositorio remoto, somente a branch main==="
 git push origin main
