bf76f50d7e84b9fc56fedf3b126833ed697ed9b8
Muriszk <murilo.suzuki@sptech.school>
2025-11-30 21:28:56 -0300

    Rpg - Alterações em textos / Dashboard - Alteração de descrição do gráfico

--- dorohedoro/src/database/dorohedoroDatabase.sql
+++ dorohedoro/src/database/dorohedoroDatabase.sql
@@ -5,10 +5,8 @@ USE dorohedoro;
 
 CREATE TABLE usuario(
 idUsuario INT PRIMARY KEY AUTO_INCREMENT,
-nomeUsuario VARCHAR(45),
 emailUsuario VARCHAR(45),
 senhaUsuario VARCHAR(45),
-recuperarSenhaUsuario VARCHAR(45)
 );
 
 CREATE TABLE volume (
@@ -28,17 +26,6 @@ textoAvaliacao VARCHAR(45),
 FOREIGN KEY (idVolume) REFERENCES volume(idVolume),
 FOREIGN KEY (idUsuario) REFERENCES usuario(idUsuario)
 );
-
-
-CREATE TABLE acessoVolume (
-    idAcesso INT PRIMARY KEY AUTO_INCREMENT,
-    idVolume INT,
-    idUsuario INT,
-    momento DATETIME DEFAULT NOW(),
-    FOREIGN KEY (idVolume) REFERENCES volume(idVolume),
-    FOREIGN KEY (idUsuario) REFERENCES usuario(idUsuario)
-);
-
 
 SELECT * FROM usuario;
 
